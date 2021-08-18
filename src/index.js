018e47d5c2b131c0999e95dc1fd8ce07044fbf4c
sandeep <sandeepprasad5678@gmail.com>
2021-08-18 21:28:54 +0530

    PWA build

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,3 +11,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+serviceWorkerRegistration.register();
